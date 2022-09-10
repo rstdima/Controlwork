0e738eab51807058778619f9392abcb5da8b289e
“Dmitriy” <colnce1-1@ya.ru>
2022-09-11 00:10:41 +0300

    add program forming a new array of strings

---
+++ task_1/Program.cs
@@ -0,0 +1,30 @@
+﻿
+// Console.Write("Введите число и текст через запятую: ");
+// string[] array1 = Console.ReadLine().Split(',');
+// string[] array2 = new string[array1.Length];
+
+string[] array1 = new string[5] { "Hello", "2", "world", ",", ":)" };
+string[] array2 = new string[array1.Length];
+
+void SecondArray(string[] array1, string[] array2)
+{
+   int count = 0;
+   for (int i = 0; i < array1.Length; i++)
+   {
+      if (array1[i].Length <= 3)
+      {
+         array2[count] = array1[i];
+         count++;
+      }
+   }
+}
+void PrintArray(string[] array)
+{
+   for (int i = 0; i < array.Length; i++)
+   {
+      Console.Write($"{array[i]} ");
+   }
+   Console.WriteLine();
+}
+SecondArray(array1, array2);
+PrintArray(array2);
